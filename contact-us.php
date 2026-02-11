124a162068c5420d35116dd23d301e812fdd495d
AnkitSinghKardam <kardamweb@gmail.com>
2026-02-11 14:57:07 +0530

    Added php code

---
+++ contact-us.php
@@ -0,0 +1,34 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "yourgmail@gmail.com";  // 👈 apna Gmail yaha daalo
+
+    $email_subject = "Website Contact: " . $subject;
+
+    $body = "
+    You have received a new message:
+
+    Name: $name
+    Email: $email
+    Subject: $subject
+    Message:
+    $message
+    ";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    if (mail($to, $email_subject, $body, $headers)) {
+        echo "<div class='alert alert-success'>Message Sent Successfully!</div>";
+    } else {
+        echo "<div class='alert alert-danger'>Failed to Send Message.</div>";
+    }
+}
+?>
---
+++ send-mail.php
@@ -0,0 +1,30 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $service = htmlspecialchars($_POST['service']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "yourgmail@gmail.com";   // 👈 apna Gmail yaha likho
+    $subject = "New Quote Request From Website";
+
+    $body = "
+    Name: $name
+    Email: $email
+    Service: $service
+    Message: $message
+    ";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<div class='alert alert-success'>Message Sent Successfully!</div>";
+    } else {
+        echo "<div class='alert alert-danger'>Failed to Send Message.</div>";
+    }
+}
+?>
